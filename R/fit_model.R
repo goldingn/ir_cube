87fe3acf00ced7c0f865c3fa276b0a1713735a58
Nick Golding <goldingn35@gmail.com>
2024-06-16 22:45:33 +0800

    get model working with greta.dynamics (time-constant selection)

--- R/fit_model.R
+++ R/fit_model.R
@@ -6,7 +6,7 @@ source("R/functions.R")
 
 # load covariate rasters
 
-# load and temporally average raster data
+# load, temporally average, and scale net coverage data
 nets_cube <- rast("data/clean/nets_per_capita_cube.tif")
 nets_flat <- terra::app(nets_cube, mean)
 names(nets_flat) <- "itn_percap"
@@ -17,6 +17,8 @@ covs_flat_orig <- rast("data/clean/flat_covariates.tif")
 
 # add on the scaled, flat nets layer
 covs_flat <- c(nets_flat_std, covs_flat_orig)
+
+mask <-  covs_flat[[1]] * 0
 
 # load data
 ir_mtm_africa <- readRDS(file = "data/clean/mtm_data.RDS")
@@ -34,15 +36,7 @@ df <- ir_mtm_africa %>%
     insecticide_class %in% c("Pyrethroids",
                              "Carbamates",
                              "Organochlorines",
-                             "Organophosphates")
-  ) %>%
-  mutate(
-    # convert concentrations into numeric values
-    concentration = as.numeric(str_remove(insecticide_conc, "%")),
-    # create a time index, relative to the baseline year
-    time = year_start - baseline_year
-  ) %>%
-  filter(
+                             "Organophosphates"),
     # drop Dieldrin as it has two concentrations in the dataset, and those with
     # fewer than 200 observations (manually as I'm being lazy)
     !(insecticide_type %in% c("Dieldrin",
@@ -53,16 +47,30 @@ df <- ir_mtm_africa %>%
     # drop any from before when we have data on net coverage
     year_start >= baseline_year
   ) %>%
-  # extract non-temporal covariates at coordinates
-  bind_cols(
-    terra::extract(covs_flat,
-                   select(., longitude, latitude) %>%
-                     as.matrix())
+  mutate(
+    # convert concentrations into numeric values
+    concentration = as.numeric(str_remove(insecticide_conc, "%")),
+    # create an index to the simulation year (in 1-indexed integers)
+    year_id = year_start - baseline_year + 1,
+    # add on cell ids corresponding to these observations,
+    cell = cellFromXY(mask, 
+                      as.matrix(select(., longitude, latitude)))
   ) %>%
-  # drop a handful of points mossing covariates
+  # drop a handful of datapoints missing covariates
   filter(
-    !is.na(itn_percap)
+    !is.na(extract(mask, cell)[, 1])
+  ) %>%
+  # add an index to the vector of unique cells (now they have been subsetted)
+  mutate(
+    cell_id = match(cell, unique(cell))
   )
+
+# pull covariates for all unique cells
+unique_cells <- unique(df$cell)
+n_unique_cells <- length(unique_cells)
+
+# create design matrix at all unique cells
+x_cells <- extract(covs_flat, unique_cells)
 
 # # extract spatiotemporal covariates from the cube
 # nets_lookup <- terra::extract(nets_cube, coords) %>%
@@ -77,31 +85,6 @@ df <- ir_mtm_africa %>%
 #   mutate(
 #     year = as.numeric(year)
 #   )
-
-# # summarise the data
-# df %>%
-#   select(insecticide_class,
-#          insecticide_type,
-#          concentration,
-#          test_type) %>%
-#   group_by(insecticide_class,
-#            insecticide_type,
-#            concentration,
-#            test_type) %>%
-#   summarise(
-#     count = n()
-#   ) %>%
-#   arrange(desc(count),
-#           insecticide_class,
-#           insecticide_type)
-
-# which covariates to use (all by default)
-covariate_names <- names(covs_flat)
-
-# create design matrix
-x <- df %>%
-  select(any_of(covariate_names)) %>%
-  as.matrix()
 
 # map the data to the insecticide classes and types
 classes <- unique(df$insecticide_class)
@@ -127,9 +110,10 @@ type_concentrations <- df %>%
   
 # dimensions
 n_obs <- nrow(df)
-n_covs <- ncol(x)
+n_covs <- ncol(x_cells)
 n_classes <- length(classes)
 n_types <- length(types)
+n_times <- max(df$year_start) - min(df$year_start) + 1
 
 # hierarchical regression parameters
 
@@ -165,49 +149,59 @@ beta_type <- beta_class[, classes_index] + beta_type_sigma
 # beta_type <- beta_type_raw
 
 
-# multiply through to get log relative fitness of resistance for each
-# insecticide type
+# multiply through to get relative fitness of resistance for each insecticide
+# type at each cell
 
-# convert beta to positive effect sizes
+# convert beta to positive effect sizes, them matrix multiply with indicator
+# variables to constrain to positive effects of the covariates on selection
+# coefficient/relative fitness
 effect_type <- exp(beta_type)
 
-# this way is marginally faster and probably quite a lot less memory intensive
-# than the matrix multiply
-effect_mat <- t(effect_type)[df$type_id,]
-eta_mat <- x * effect_mat
-eta_vec <- greta:::rowSums(eta_mat)
+# # compute selection coefficients, convert to relative fitness, and extract at
+# # data locations
+selection_cells <- x_cells %*% effect_type
+fitness_cells <- 1 + selection_cells
 
-# pull out the etas (observations, by insecticides) corresponding to data
-# eta <- x %*% effect_type
-# data_index <- cbind(seq_len(n_obs), df$type_id)
-# eta_vec <- eta[data_index]
-
-# model fractions susceptible in 2000
-
-# # hierarchical prior (with hierarchical decentring) on the 
-# logit_init_overall <- normal(qlogis(0.999), 1)
-# logit_init_sd <- normal(0, 0.5, truncation = c(0, Inf))
-# logit_init_raw <- normal(0, 1, dim = n_types)
-# logit_init <- logit_init_overall + logit_init_raw * logit_init_sd
-# init_fraction_susceptible <- ilogit(logit_init)
-
-# more straightforward prior with a harder limit
+# model fractions susceptible prior to 2000
 init_fraction_susceptible <- normal(1, 0.01,
                                     dim = n_types,
                                     truncation = c(0, 1))
-# sims <- calculate(init_fraction_susceptible[1], nsim = 1e4)[[1]]
-# hist(sims, xlim = c(0.8, 1))
-# range(sims)
-# compute the fraction susceptible over time against each insecticide
 
-# more stable version of this:
-#   fitness = exp(eta)
-#   cumulative_fitness = fitness ^ df$time
-cumulative_fitnesses_vec <- exp(eta_vec * df$time)
+# compute the fraction susceptible over time against each insecticide via
+# haploid trait evolution (equivalent to diploid evolution with heterozygote
+# dominance = 0.5)
 
-init_vec <- init_fraction_susceptible[df$type_id]
-fraction_susceptible_vec <- init_vec / 
-  (init_vec + (1 - init_vec) * cumulative_fitnesses_vec)
+# solve through time to get fraction susceptible over time
+haploid_next <- function(state, iter, w) {
+  p <- state
+  q <- 1 - p
+  p / (p + q * w)
+}
+
+# expand initial conditions out to all cells with data (plus a trailing
+# dimension to match greta.dynamics interface)
+init_cells <- sweep(ones(n_unique_cells, n_types),
+                    2,
+                    init_fraction_susceptible,
+                    FUN = "*")
+dim(init_cells) <- c(dim(init_cells), 1)
+
+# pad out the dimension of the fitness to match
+dim(fitness_cells) <- c(dim(fitness_cells), 1)
+
+# iterate through time to get fraction susceptible for all years at all cells
+# with data
+dynamic_cells <- iterate_dynamic_function(
+  transition_function = haploid_next,
+  initial_state = init_cells,
+  niter = n_times,
+  w = fitness_cells,
+  tol = 0)
+
+# pull out the values at the cells, insecticides, and years corresponding to the
+# data
+index <- cbind(df$cell_id, df$type_id, df$year_id)
+fraction_susceptible_vec <- dynamic_cells$all_states[index]
 
 # model the susceptible & resistant LD50s of the different insecticides
 # (skip this until we have multiple concentrations)
@@ -241,31 +235,27 @@ fraction_susceptible_vec <- init_vec /
 # for now, map straight from genotype to mortality
 population_mortality_vec <- fraction_susceptible_vec
 
-# define betabinomial observation model
+# define observation model
 
-# need to constrain this error for now, until there are covariates to explain it
-# better
-# observation_extra_error <- normal(0, 0.1, truncation = c(0, Inf))
-
-# compute the expected sd of the binomial (assuming independent samples)
+# compute the expected sd of a binomial distribution (ie. assuming independent
+# samples)
 binomial_sd <- sqrt(df$mosquito_number *
                       population_mortality_vec *
                       (1 - population_mortality_vec))
 
-# model the observation (betabinomial) sd as a multiplier on the binomial sd
+# model the observation (betabinomial) sd as a multiplier on the binomial sd,
+# accounting for additional error due to nonindependent sampling of individuals
+# from the population
 observation_sd_multiplier_raw <- normal(0, 0.1, truncation = c(0, Inf))
 observation_sd_multiplier <- 1 + observation_sd_multiplier_raw
-
-# summary(calculate(observation_sd_multiplier, nsim = 1e6)[[1]])
-# observation_sd_multiplier <- 1
 observation_sd <- binomial_sd * observation_sd_multiplier
 
+# compute parameters of the beta-binomial that match this mean and sd
 a <- ((1 - population_mortality_vec) / observation_sd ^ 2 - 1 / population_mortality_vec) * population_mortality_vec ^ 2
 b <- a * (1 / population_mortality_vec - 1)
-distribution(df$died) <- beta_binomial(df$mosquito_number, a, b)
 
-# or just use binomial
-# distribution(df$died) <- binomial(df$mosquito_number, population_mortality_vec)
+# define the distribution over the observations
+distribution(df$died) <- beta_binomial(df$mosquito_number, a, b)
 
 m <- model(
   beta_overall,
@@ -290,8 +280,8 @@ system.time(
                 initial_values = inits,
                 chains = n_chains)
 )
-# user  system elapsed 
-# 605.339  71.699 162.177 
+# user   system  elapsed 
+# 4243.033 2195.452 1795.518 
 
 # check convergence
 coda::gelman.diag(draws,
@@ -312,23 +302,25 @@ plot(frac_susc_post_mean ~ jitter(df$year_start),
 # calculate expected bioassay mortalities over time for each insecticide, with
 # fixed assumption about covariate values, and overplot the data
 
-n_plot <- 100
-type_idx <- 1
-times_plot <- seq(1990, max(df$year_start), length.out = n_plot)
-time_diff_plot <- sweep(matrix(times_plot, n_plot, n_types), 2, 2000, FUN = "-")
-
 x_fixed <- t(c(0.5, rep(0, n_covs - 1)))
-eta_plot <- x_fixed %*% exp(beta_type)
-cumulative_fitnesses_plot <- exp(sweep(time_diff_plot, 2, t(eta_plot), FUN = "*"))
+selection_plot <- t(x_fixed %*% exp(beta_type))
+fitness_plot <- 1 + selection_plot
+times_plot <- baseline_year + seq_len(n_times) - 1
 
-# compute the fraction susceptible over time against each insecticide
-init_mat <- sweep(zeros(n_plot, n_types), 2, init_fraction_susceptible, FUN = "+")
-fraction_susceptible_plot <- init_mat /
-  (init_mat + (1 - init_mat) * cumulative_fitnesses_plot)
+dynamic_outputs_plot <- iterate_dynamic_function(
+  transition_function = haploid_next,
+  initial_state = init_fraction_susceptible,
+  niter = n_times,
+  w = fitness_plot,
+  tol = 0)
+
+# pull out the data locations
+fraction_susceptible_plot <- t(dynamic_outputs_plot$all_states)
 population_mortality_plot <- fraction_susceptible_plot
 
 post_plot_sims <- calculate(
   population_mortality_plot,
+  fitness_plot,
   values = draws,
   nsim = 1000)
 
@@ -366,7 +358,7 @@ for (i in seq_len(n_types)) {
 
 # plot posteriors
 posteriors <- calculate(init_fraction_susceptible,
-                        beta_type,
+                        effect_type,
                         observation_sd_multiplier,
                         values = draws,
                         nsim = 1000)
@@ -385,9 +377,10 @@ for (i in seq_len(n_types)) {
   
 }
 
+# only shown for ITNs
 par(mfrow = n2mfrow(n_types))
 for (i in seq_len(n_types)) {
-  hist(posteriors$beta_type[, , i],
+  hist(posteriors$effect_type[, 1, i],
        breaks = 100,
        main = types[i])
   
@@ -395,8 +388,8 @@ for (i in seq_len(n_types)) {
 
 # make prediction rasters
 
-# get all cells in mastergrids, and extract the values there
-cells <- terra::cells(covs_flat[[1]])
+# get all cells in mastergrids mask, and extract the values there
+cells <- terra::cells(mask)
 
 # nets_cube_dataframe <- terra::extract(nets_cube, cells) %>%
 #   mutate(
@@ -411,14 +404,12 @@ cells <- terra::cells(covs_flat[[1]])
 #     year = as.numeric(year)
 #   )
 
-x_predict <- terra::extract(covs_flat, cells) %>%
-  as_tibble() %>%
-  select(any_of(covariate_names)) %>%
-  as.matrix()
+x_predict <- terra::extract(covs_flat, cells)
 
 # years to predict to
 years_predict <- 2000:2030
-times_predict <- years_predict - baseline_year
+
+n_times_predict <- length(years_predict)
 
 # loop through insecticides making predictions in batches of cells for multiple years simultaneously
 template_raster <- nets_cube$nets_2000 * 0
@@ -434,13 +425,12 @@ names(template_cube) <- years_predict
 
 # NOTE due to a weird-as-hell greta bug, this object must not be called
 # batch_size: https://github.com/greta-dev/greta/issues/634
-# batch_bigness <- length(cells) + 1
 batch_bigness <- 1e5
 batch_idx <- seq_along(cells) %/% batch_bigness
 # index to raster, for setting cell values
 cell_batches <- split(cells, batch_idx)
 # index to x_predict
-x_batches <- split(seq_along(cells), batch_idx)
+x_row_batches <- split(seq_along(cells), batch_idx)
 n_batches <- length(cell_batches)
 
 # insecticide types to save
@@ -466,42 +456,38 @@ for (this_insecticide in types_save) {
                   n_batches))
     
     cell_batch <- cell_batches[[batch_index]]
-    x_batch <- x_batches[[batch_index]]
+    x_row_batch <- x_row_batches[[batch_index]]
+    batch_n <- length(cell_batch)
     
+    # for this insecticide, tile the intial conditions for each cell
     type_id_predict <- match(this_insecticide, types)
+    init_batch <- init_fraction_susceptible[type_id_predict] * ones(batch_n)
     
     # get log fitness for population resistant to this insecticide, at these
     # cells
-    x_predict_batch <- x_predict[x_batch, ]
-    effect_type <- exp(beta_type)
-    eta_predict <- x_predict_batch %*% effect_type[, type_id_predict]
+    x_batch <- x_predict[x_row_batch, ]
+    selection_batch <- x_batch %*% effect_type[, type_id_predict]
+    fitness_batch <- 1 + selection_batch
+    dynamics_batch <- iterate_dynamic_function(
+      transition_function = haploid_next,
+      initial_state = init_batch,
+      niter = n_times_predict,
+      w = fitness_batch,
+      tol = 0)
     
-    # accumulate over time and convert to fitness
-    times_predict_mat <- outer(rep(1, length(cell_batch)),
-                               times_predict,
-                               FUN = "*")
-    cumulative_eta_predict <- sweep(as_data(times_predict_mat),
-                                    1,
-                                    eta_predict,
-                                    FUN = "*")
-    cumulative_fitnesses_predict <- exp(cumulative_eta_predict)
-    
-    # compute the fraction susceptible to this insecticide over time
-    init_predict <- init_fraction_susceptible[type_id_predict]
-    fraction_susceptible_predict <- init_predict / 
-      (init_predict + (1 - init_predict) * cumulative_fitnesses_predict)
-    population_mortality_predict <- fraction_susceptible_predict
+    fraction_susceptible_batch <- dynamics_batch$all_states
+    population_mortality_batch <- fraction_susceptible_batch
     
     # get posterior draws of these, fixing the RNG seed so it's the same
     # collection of posterior samples for all batches
     # set.seed(this_seed)
-    pred_mat <- calculate(population_mortality_predict,
+    pred_batch <- calculate(population_mortality_batch,
                           values = draws,
                           seed = this_seed,
                           nsim = 1e2)[[1]]
     
-    # compute posterior mean
-    batch_pred_mean <- apply(pred_mat, 2:3, mean)
+    # compute posterior mean over the draws, for cells and years
+    batch_pred_mean <- apply(pred_batch, 2:3, mean)
     
     # stick this batch of predictions in the raster cube, one year at a time
     for (y in seq_along(years_predict)) {
