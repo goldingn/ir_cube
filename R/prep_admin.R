79d00d8a4ce8a626aeccdd326906b05d153ed039
Nick Golding <goldingn35@gmail.com>
2024-11-04 13:24:36 +0800

    fill in missing cell values in country/region rasters, rebuild plots

--- R/prep_admin.R
+++ R/prep_admin.R
@@ -48,6 +48,47 @@ gadm_raster_country <- gadm_in_mask %>%
 gadm_raster_region <- gadm_in_mask %>%
   terra::rasterize(mask, field = "region")
 
+# there are some cells missing from the raster, where they do not overlap with
+# the countries
+nrow(extract(mask, cells(mask)))
+nrow(extract(gadm_raster_country, cells(gadm_raster_country)))
+nrow(extract(gadm_raster_region, cells(gadm_raster_region)))
+
+# find these cells
+missing_cell_coords <- gadm_raster_country %>%
+  extract(cells(mask), xy = TRUE) %>%
+  filter(is.na(country_name)) %>%
+  select(-country_name)
+
+# find the nearest country/region to each cell
+nearest_link <- missing_cell_coords %>%
+  vect(geom = c("x", "y"),
+       crs = crs(gadm_in_mask)) %>%
+  nearest(vect(gadm_in_mask))
+nearest_country <- gadm_in_mask$country_name[nearest_link$to_id]
+nearest_region <- gadm_in_mask$region[nearest_link$to_id]
+
+# find the corresponding codes
+gadm_country_levels <- levels(gadm_raster_country)[[1]]
+nearest_country_id <- gadm_country_levels$ID[match(nearest_country, gadm_country_levels$country_name)]
+gadm_region_levels <- levels(gadm_raster_region)[[1]]
+nearest_region_id <- gadm_region_levels$ID[match(nearest_region, gadm_region_levels$region)]
+
+# put the values back in
+missing_cell_index <- terra::cellFromXY(mask,
+                                        missing_cell_coords)
+gadm_raster_country[missing_cell_index] <- nearest_country_id
+gadm_raster_region[missing_cell_index] <- nearest_region_id
+
+# remask, for whatever reason
+gadm_raster_country <- mask(gadm_raster_country, mask)
+gadm_raster_region <- mask(gadm_raster_region, mask)
+
+# check they are all filled in now
+nrow(extract(mask, cells(mask)))
+nrow(extract(gadm_raster_country, cells(gadm_raster_country)))
+nrow(extract(gadm_raster_region, cells(gadm_raster_region)))
+
 writeRaster(gadm_raster_country,
             "data/clean/country_raster.tif",
             overwrite = TRUE)
