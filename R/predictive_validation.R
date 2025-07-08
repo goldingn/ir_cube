c9854c28398414aedcbdaae263a37510c7b903f4
Nick Golding <goldingn35@gmail.com>
2025-07-08 13:26:02 +0800

    implement null model

--- R/predictive_validation.R
+++ R/predictive_validation.R
@@ -351,3 +351,306 @@ spatial_extrapolation
 spatial_interpolation
 temporal_forecasting
 
+
+# Define the null model: For each point in the training data, average over the X
+# nearest datapoints from the current and previous year
+
+rmse <- function(observed, predicted) {
+  sqrt(mean(observed - predicted) ^ 2)
+}
+
+mae <- function(observed, predicted) {
+  mean(abs(observed - predicted))
+}
+
+# given vectors of latitude, longitude, year, and insecticide type for test
+# data, and a tibble of training data, return a vector of predictions of the
+# susceptibility fraction from a weighted average of the `n_nearest_neighbours`
+# nearest points in the training data of that insecticide type, and in the same
+# year or up to `n_years_prior` earlier years
+predict_null_fixed_nn <- function(latitude,
+                                  longitude,
+                                  year,
+                                  insecticide_type,
+                                  training_data,
+                                  n_nearest_neighbours,
+                                  n_years_prior = 1) {
+  
+  training_coords <- training_data %>%
+    select(longitude, latitude) %>%
+    as.matrix()
+  
+  test_coords <- bind_cols(longitude = longitude,
+                           latitude = latitude) %>%
+    as.matrix()
+  
+  # compute the distance matrix between the test and training sets
+  dists <- fields::rdist.earth(test_coords,
+                               training_coords,
+                               miles = FALSE)
+  
+  year_diff <- -1 * seq(0, n_years_prior)
+  
+  n_test <- nrow(test_coords)
+  preds <- rep(NA, n_test)
+  # loop through each test record
+  for (i in seq_len(n_test)) {
+    # obtain the vector of distances to that record from all training records
+    distance_vec <- dists[i, ]
+    
+    # mask these (with Inf) if they are not in the same or the previous year or
+    # not for the same insecticide type
+    this_year <- year[i]
+    this_insecticide_type <- insecticide_type[i]
+    valid_years <- this_year + year_diff
+    training_year_valid <- training$year_start %in% valid_years
+    insecticide_type_valid <- training$insecticide_type == this_insecticide_type
+    valid <- training_year_valid & insecticide_type_valid
+    masked_distance_vec <- ifelse(valid, distance_vec, Inf)
+    
+    # identify the X closest records
+    threshold_distance <- sort(masked_distance_vec,
+                               decreasing = FALSE)[n_nearest_neighbours]
+    nearest_training_idx <- which(masked_distance_vec <= threshold_distance)
+    
+    # compute a weighted mean of these as the prediction
+    total_died <- sum(training_data$died[nearest_training_idx])
+    total_tested <- sum(training_data$mosquito_number[nearest_training_idx])
+    preds[i] <- total_died / total_tested
+    
+  }
+  
+  # return the predictions
+  preds
+  
+}
+
+
+# given tibbles of test and training data, return the test data tibble augmented
+# with observed and predicted values of the susceptibility fraction from a
+# weighted average of the nearest points in the training data of that
+# insecticide type, and in the same year or up to `n_years_prior` earlier years,
+# for multiple values of the number of neighbours, across a grid search between
+# the values in `n_nearest_neighbour_range`, evaluating integers
+# `n_nearest_neighbour_delta` apart. If plot = TRUE, plot RMSE against the
+# numbers of neighbours for visual assessment of convexity. The number of
+# neighbours yielding the minimal RMSE is identified by the column
+# `nn_is_optimal`; filtering on this column will yield the optimal predictions
+predict_null_optimal_nn <- function(test_data,
+                                    training_data,
+                                    n_nearest_neighbour_range = c(1, 20),
+                                    n_nearest_neighbour_delta = 1,
+                                    n_years_prior = 1,
+                                    plot = TRUE) {
+  
+  # Do grid search to find the optimal number of nearest neighbours for spatial
+  # interpolation
+  
+  # nearest neighbour values to try
+  nn_values <- seq(from = n_nearest_neighbour_range[1],
+                   to = n_nearest_neighbour_range[2],
+                   by = n_nearest_neighbour_delta)
+  
+  # test data
+  grid_search <- test_data %>%
+    mutate(
+      observed = died / mosquito_number
+    ) %>%
+    # add on nearest neighbour values to try (enforce integers)
+    expand_grid(
+      n_neighbours = round(nn_values)
+    ) %>%
+    # batch predictions by each value of the neighbour parameter
+    group_by(
+      n_neighbours
+    ) %>%
+    # compute predictions and observed fractions
+    mutate(
+      predicted = predict_null_fixed_nn(longitude = longitude,
+                                        latitude = latitude,
+                                        year = year_start,
+                                        insecticide_type = insecticide_type,
+                                        training = spatial_interpolation$training,
+                                        n_nearest_neighbours = n_neighbours[1])
+    ) %>%
+    ungroup()
+  
+  # compute rmse for this grid search on numbers of neighbours to find the optimum
+  rmses <- grid_search %>%
+    group_by(
+      n_neighbours
+    ) %>%
+    summarise(
+      rmse = rmse(observed, predicted)
+    )
+  
+  # maybe plot the relationship to check for convexity
+  if (plot) {
+    plot(rmse ~ n_neighbours,
+         data = rmses,
+         type = "b")
+  }
+  
+  # pull out the optimal value
+  optimal_nn <- rmses %>%
+    filter(rmse == min(rmse)) %>%
+    slice(1) %>%
+    pull(n_neighbours)
+  
+  # add a flag for optimality and return
+  grid_search %>%
+    mutate(
+      nn_is_optimal = n_neighbours == optimal_nn
+    )
+  
+}
+
+
+# Do grid search to find the optimal number of nearest neighbours for spatial
+# interpolation and temporal forecasting
+
+spatial_interpolation_preds <- spatial_interpolation$test %>%
+  mutate(
+    mortality = died / mosquito_number
+  ) %>%
+  predict_null_optimal_nn(
+    training_data = spatial_interpolation$training
+  )
+
+# for temporal forecasting, use up to 3 years prior to enable prediction to the
+# third year into the future
+temporal_forecasting_preds <- temporal_forecasting$test %>%
+  mutate(
+    mortality = died / mosquito_number
+  ) %>%
+  predict_null_optimal_nn(
+    training_data = temporal_forecasting$training,
+    n_years_prior = 3
+  )
+
+# for spatial extrapolation, need to run it for each country and compute the average rmses to
+# RMSEs to identify the optimal number of neighbours
+each_country_optimal_nn <- function(test, training) {
+  test %>%
+    mutate(
+      mortality = died / mosquito_number
+    ) %>%
+    predict_null_optimal_nn(
+      training_data = training
+    )
+}
+
+spatial_extrapolation_preds <- mapply(
+  FUN = each_country_optimal_nn,
+  spatial_extrapolation$test,
+  spatial_extrapolation$training,
+  SIMPLIFY = FALSE
+) %>%
+  do.call(
+    bind_rows, .
+  )
+
+# get and plot the overall rmses
+spatial_extrapolation_rmses <- spatial_extrapolation_preds %>%
+  group_by(
+    n_neighbours
+  ) %>%
+  summarise(
+    rmse = rmse(observed, predicted)
+  )
+
+plot(rmse ~ n_neighbours,
+     data = spatial_extrapolation_rmses,
+     type = "b")
+
+# find the optimum
+spatial_extrapolation_optimal_nn <- spatial_extrapolation_rmses %>%
+  filter(rmse == min(rmse)) %>%
+  slice(1) %>%
+  pull(n_neighbours)
+
+# overwrite the optimal flag
+spatial_extrapolation_preds <- spatial_extrapolation_preds %>%
+  mutate(
+    nn_is_optimal = n_neighbours == spatial_extrapolation_optimal_nn
+  )
+
+# subset each of these validation experiments to the optimal numbers of
+# neighbours
+optimal_nn_preds <- bind_rows(
+  spatial_interpolation = spatial_interpolation_preds,
+  spatial_extrapolation = spatial_extrapolation_preds,
+  temporal_forecasting = temporal_forecasting_preds,
+  .id = "experiment"
+) %>%
+  filter(
+    nn_is_optimal
+  )
+
+# record the optima
+optimal_nn <- optimal_nn_preds %>%
+  group_by(
+    experiment
+  ) %>%
+  summarise(
+    n_neighbours = n_neighbours[1]
+  )
+optimal_nn
+
+# compute bias and RMSE for each of these and tabulate
+optimal_nn_preds %>%
+  group_by(
+    experiment
+  ) %>%
+  summarise(
+    rmse = rmse(observed, predicted),
+    mae = mae(observed, predicted),
+    bias = mean(predicted - observed),
+    .groups = "drop"
+  )
+
+# split these by country (spatial extrapolation) and years ahead (temporal
+# forecasting)
+optimal_nn_preds %>%
+  filter(
+    experiment == "spatial_extrapolation"
+  ) %>%
+  group_by(
+    country_name
+  ) %>%
+  summarise(
+    rmse = rmse(observed, predicted),
+    mae = mae(observed, predicted),
+    bias = mean(predicted - observed),
+    .groups = "drop"
+  )
+
+# The biases are positive for some countries and negative for others. They
+# cancel out in aggregate, but this is suboptimal
+
+
+optimal_nn_preds %>%
+  filter(
+    experiment == "temporal_forecasting"
+  ) %>%
+  group_by(
+    year_start
+  ) %>%
+  summarise(
+    rmse = rmse(observed, predicted),
+    mae = mae(observed, predicted),
+    bias = mean(predicted - observed),
+    .groups = "drop"
+  )
+
+# these are substantially more biased further into the future, as expected
+
+
+# should these validation studies be limited to dropouts in recent years, to
+# capture heterogeneity and average levels of resistance?
+
+# should the error and bias measures attempt to account for the binomial
+# variance (errors will be bigger closer to 0.5, so scale according to this?)
+
+# Can we use Penny's previously estimated maps (to 2017) in this comparison?
+# https://doi.org/10.6084/m9.figshare.9912623 
