27ced744dda257f0048a3b17a05e7a7e572b9a02
AugustHao <tianxiao.hao@telethonkids.org.au>
2025-08-26 11:20:53 +1000

    add temporal forecast validation for dynamical model

--- R/CV_dummy.R
+++ R/CV_dummy.R
@@ -563,7 +563,7 @@ write_csv(dynamic_extrap_result_vs_hancock_joined,"outputs/dynamic_extrap_result
     draws <- mcmc(m,
                   chains = n_chains,
                   n_samples = 1e3,
-                  warmup = 500)
+                  warmup = 1e3)
   )
   
   
@@ -662,5 +662,229 @@ write_csv(dynamic_extrap_result_vs_hancock_joined,"outputs/dynamic_extrap_result
   gc()
 
 
+  #####do temporal forecast
+  
+  
+  # hierarchical regression parameters
+  
+  # doubly hierarchical version
+  
+  # between-classes
+  beta_overall <- normal(0, 1, dim = n_covs)
+  sigma_overall <- normal(0, 1, dim = n_covs, truncation = c(0, Inf))
+  sigma_class <- normal(0, 1, dim = n_covs, truncation = c(0, Inf))
+  
+  # between-types
+  beta_class_raw <- normal(0, 1, dim = c(n_covs, n_classes))
+  beta_class_sigma <- sweep(beta_class_raw, 1, sigma_overall, FUN = "*")
+  beta_class <- sweep(beta_class_sigma, 1, beta_overall, FUN = "+")
+  
+  # betas for types
+  beta_type_raw <- normal(0, 1, dim = c(n_covs, n_types))
+  beta_type_sigma <- sweep(beta_type_raw, 1, sigma_class, FUN = "*")
+  beta_type <- beta_class[, classes_index] + beta_type_sigma
+  
+  # multiply through to get relative fitness of resistance for each insecticide
+  # type at each cell
+  
+  effect_type <- exp(beta_type)
+  
+  
+  selection_cell_years <- x_cell_years %*% effect_type
+  fitness_cell_years <- 1 + selection_cell_years
+  
+  # reformat this in to a 3D array with dimensions:
+  #   n_times x n_unique_cells x n_types x 1
+  # to solve dynamics with time-varying fitness (time must be first, then other
+  # two must match state variable, which has a trailing dimension of size 1)
+  fitness_array <- fitness_cell_years
+  dim(fitness_array) <- c(n_times, n_unique_cells, n_types, 1)
+  
+  # prior and minimum values for the initial fractions susceptible
+  init_frac_prior <- ifelse(types == "DDT", 0.9, 0.95)
+  init_frac_min <- ifelse(types == "DDT", 0.75, 0.9)
+  
+  # mean logit proportion of the distance from the minimum to 1, for each type
+  init_frac_relative_prior <- (init_frac_prior - init_frac_min) / (1 - init_frac_min)
+  logit_init_mean <- normal(qlogis(init_frac_relative_prior), 1, dim = n_types)
+  
+  # variance at each level, for each insecticide
+  init_region_sd <- normal(0, 1, truncation = c(0, Inf), dim = n_types)
+  init_country_sd <- normal(0, 1, truncation = c(0, Inf), dim = n_types)
+  
+  # unscaled deviation parameters (hierarchical decentring)
+  init_region_raw <- normal(0, 1, dim = c(n_regions, dim = n_types))
+  init_country_raw <- normal(0, 1, dim = c(n_countries, dim = n_types))
+  
+  # combine to get the regional and country-level deviation from the prior logit
+  # mean
+  init_region_effect <- sweep(init_region_raw, 2, init_region_sd, FUN = "*")
+  init_country_effect <- sweep(init_country_raw, 2, init_country_sd, FUN = "*")
+  
+  country_region_index <- df %>%
+    group_by(country_id) %>%
+    slice(1) %>%
+    ungroup() %>%
+    select(country_id, region_id) %>%
+    arrange(country_id) %>%
+    pull(region_id)
+  
+  # combine these together into the country-level logit-mean initial value
+  # logit_init_mean <- qlogis(init_frac_mean)
+  init_country_overall_effect <- init_country_effect + init_region_effect[country_region_index, ]
+  logit_init_country <- sweep(init_country_overall_effect,
+                              2,
+                              logit_init_mean,
+                              FUN = "+")
+  
+  # convert from relative (0-1) to the constrained scale (above init_frac_min)
+  init_country_relative <- ilogit(logit_init_country)
+  init_range <- 1 - init_frac_min
+  init_country_magnitude <- sweep(init_country_relative, 2, init_range, FUN = "*") 
+  init_country <- sweep(init_country_magnitude, 2, init_frac_min, FUN = "+")
+  
+  cell_country_lookup <- df %>%
+    group_by(cell_id) %>%
+    slice(1) %>%
+    ungroup() %>%
+    select(cell_id, country_id) %>%
+    arrange(cell_id) %>%
+    pull(country_id)
+  
+  # expand out to all observations
+  init_array <- init_country[cell_country_lookup, ]
+  
+  # add a trailing dimension to match greta.dynamics interface
+  dim(init_array) <- c(dim(init_array), 1)
+  
+  # iterate through time to get fraction without resistant allele for all years at all cells
+  # with data
+  dynamic_cells <- iterate_dynamic_function(
+    transition_function = haploid_next,
+    initial_state = init_array,
+    niter = n_times,
+    w = fitness_array,
+    parameter_is_time_varying = c("w"),
+    tol = 0)
+  
+  # pull out the values at the cells, insecticides, and years corresponding to the
+  # data
+  
+  # pull out this training data
+  fold_train_df <- temporal_forecasting$training
+  
+  index <- cbind(fold_train_df$cell_id, fold_train_df$type_id, fold_train_df$year_id)
+  fraction_susceptible_vec <- dynamic_cells$all_states[index]
+  
+  # resistant phenotype expression rate = expected mortality in bioassays
+  population_mortality_vec <- fraction_susceptible_vec
+  
+  # define observation model
+  rho_classes <- normal(0, 0.025,
+                        truncation = c(0, 1),
+                        dim = n_classes)
+  
+  distribution(fold_train_df$died) <- betabinomial_p_rho(
+    N = fold_train_df$mosquito_number,
+    p = population_mortality_vec,
+    rho = rho_classes[fold_train_df$class_id])
+  
+  m <- model(
+    beta_overall,
+    sigma_overall,
+    beta_type_raw,
+    rho_classes
+  )
+  
+  n_chains <- 4
+  
+  system.time(
+    draws <- mcmc(m,
+                  chains = n_chains,
+                  n_samples = 1e3,
+                  warmup = 1e3)
+  )
+  
+  
+  fold_test_df <- temporal_forecasting$test
+  
+  index_test <- cbind(fold_test_df$cell_id, fold_test_df$type_id, fold_test_df$year_id)
+  fraction_susceptible_vec_test <- dynamic_cells$all_states[index_test]
+  population_mortality_vec_test <- fraction_susceptible_vec_test
+  
+  predicted_test <- calculate(population_mortality_vec_test,
+                              values = draws,
+                              nsim = 1e3)
+  
+  rho_classes_test <- calculate(rho_classes,
+                                values = draws,
+                                nsim = 1e3)
+  
+  predicted_test_mean <- apply(predicted_test[[1]],2:3,mean) %>% as.numeric()
+  
+  # add predicted value back to test data for bookkeeping
+  
+  temporal_forecasting$test <- tibble(temporal_forecasting$test, predicted_dynamic = predicted_test_mean)
+  
+  rho_classes_test_mean <- apply(rho_classes_test[[1]],2:3,mean) %>% as.numeric()
+  
+  rho_classes_test_indexed <- rho_classes_test_mean[fold_test_df$class_id]
+  
+  test_outcome_df <- optimal_nn_preds %>%
+    filter(
+      experiment == "temporal_forecasting"
+    ) %>% 
+    select(died,mosquito_number,observed,year_start,
+           country_name,
+           insecticide_type) %>% 
+    mutate(predicted_test_mean = predicted_test_mean,
+           rho_classes_test_indexed = rho_classes_test_indexed) %>%
+    group_by(
+      year_start,
+      country_name,
+      insecticide_type
+    ) %>% 
+    summarise(
+      pred_error_dynamic = betabinom_dev(died = died,
+                                         mosquito_number = mosquito_number,
+                                         predicted = predicted_test_mean,
+                                         rho = rho_classes_test_indexed),
+      bias_dynamic = mean(predicted_test_mean - observed),
+      .groups = "drop"
+    ) 
+  
+  
+  # result df
+  dynamic_temporal_forecast_result <- optimal_nn_preds %>%
+    filter(
+      experiment == "temporal_forecasting"
+    ) %>% 
+    group_by(year_start,
+             country_name,
+             insecticide_type) %>% 
+    summarise(
+      pred_error_nn = betabinom_dev(died = died,
+                                    mosquito_number = mosquito_number,
+                                    predicted = predicted),
+      bias_nn = mean(predicted - observed)
+    ) %>% 
+    left_join(test_outcome_df,by = join_by(year_start,
+                                           country_name,
+                                           insecticide_type)) %>% 
+    mutate(experiment = "dynmaic_temporal_forecasting")
+  
+  quick_regression <- lm(bias ~ model + country_name + insecticide_type, 
+     data = dynamic_temporal_forecast_result %>% 
+       pivot_longer(cols = starts_with("bias"),names_to = "model",values_to = "bias"))
+  
+  anova(quick_regression)
+  
+  # save to csv
+  View(dynamic_temporal_forecast_result)
+  write_csv(dynamic_temporal_forecast_result,"outputs/dynamic_temporal_forecast_result.csv")
+  
 
+  
+  purge_greta_model()
+  gc()
 
