8b95aa1f369bd3eb627cd04a018e8b839f4b8059
Nick Golding <goldingn35@gmail.com>
2025-10-17 12:10:04 +0800

    make fig2 multicolour

--- R/fig_ir_map_2025.R
+++ R/fig_ir_map_2025.R
@@ -41,16 +41,36 @@ ir_filenames <- sprintf(
 
 ir_rasts <- rast(ir_filenames)
 
+pyrethroid_names <- c("Alpha-cypermethrin",
+                      "Deltamethrin",
+                      "Lambda-cyhalothrin",
+                      "Permethrin")
+pyrethroid_star <- ifelse(insecticide_names %in% pyrethroid_names, "*", "")
+
+# make figure names, with panel letters and asterisks
 insecticide_figure_names <- paste0(
   LETTERS[2:10],
   ") ",
-  insecticide_names)
+  insecticide_names,
+  pyrethroid_star)
 
 names(ir_rasts) <- insecticide_figure_names
 
+# change the coordinates for plotting all rasters, to ensure the plots are approximately square
+new_ratio <- 0.83
+
+ext <- as.vector(ext(llin_rast))
+diffs <- diff(ext)[c(1, 3)]
+
+# keep the y limits the same, and adjust the x limits
+xmid <- ext[1] + 0.5 * diffs[1]
+xdiff <- diffs[2] / new_ratio
+
+# rebuild the limits
+xlim <- xmid + xdiff * c(-0.5, 0.5)
+
 
 # make a plot of susceptibility to Pyrethroid insecticides in 2025
-
 pyrethroid_fig <- ggplot() +
   geom_spatraster(
     data = llin_rast
@@ -62,45 +82,57 @@ pyrethroid_fig <- ggplot() +
     na.value = "transparent",
     guide = "none"
   ) +
+  # coord_sf(xlim = xlim) +
   theme_ir_maps() +
   theme(
     strip.text.x = element_text(hjust = 0)
   )
+
+# make 9 different plots, for each of the individual insecticides
+
+n_insecticides <- length(insecticide_figure_names)
+all_insecticides_fig_list <- list()
+all_insecticides_cols <- scales::hue_pal()(n_insecticides)
+
+for (i in seq_len(n_insecticides)) {
+  all_insecticides_fig_list[[i]] <- ggplot() +
+    geom_spatraster(
+      data = ir_rasts[[i]]
+    ) +
+    facet_wrap(~lyr,
+               ncol = 3) +
+    scale_fill_gradient(
+      labels = scales::percent,
+      name = "Susceptibility",
+      high = all_insecticides_cols[i],
+      guide = "none",
+      limits = c(0, 1),
+      na.value = "transparent"
+    ) +
+    coord_sf(xlim = xlim) +
+    theme_ir_maps() +
+    theme(
+      strip.text.x = element_text(hjust = 0)
+    )
+}
 
 # make a 9-panel plot of susceptibility to all insecticides in 2025
-all_insecticides_fig <- ggplot() +
-  geom_spatraster(
-    data = ir_rasts
-  ) +
-  facet_wrap(~lyr,
-             ncol = 3) +
-  guides(
-    size = guide_legend(title = "Susceptibility")
-  ) +
-  scale_fill_gradient(
-    labels = scales::percent,
-    name = "Susceptibility",
-    limits = c(0, 1),
-    na.value = "transparent"
-  ) +
-  theme_ir_maps() +
-  theme(
-    strip.text.x = element_text(hjust = 0)
-  )
-all_insecticides_fig
-
+all_insecticides_fig <- patchwork::wrap_plots(all_insecticides_fig_list,
+                                              ncol = 3) +
+  plot_layout()
 
 # combine them to plot
-pyrethroid_fig + all_insecticides_fig
+combined_fig <- pyrethroid_fig + all_insecticides_fig
 
 # save the plot
 ggsave(
-  "figures/ir_map_all_insecticides_2025.png",
+  filename = "figures/ir_map_all_insecticides_2025.png",
+  plot = combined_fig,
   bg = "white",
   width = 14,
   height = 6,
   scale = 0.8
 )
 
-# need to make colour ramps in all_insecticides_fig match the panels in figure 1
-
+# to do:
+# - add a legend on the side (wrap_plots(guides = collect) ?)
